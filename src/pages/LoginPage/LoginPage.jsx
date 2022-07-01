c6c11cc586a94f73b03e8c67c5837da752ba37f8
sokorevvvladimir <sokorevvvladimir@gmail.com>
2022-07-01 13:25:27 +0200

    stable version before moving to Redux

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,37 +1,8 @@
-import { useState } from 'react';
+import LoginForm from "../../components/LoginForm";
 
 const LoginPage = () => { 
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
 
-    const onInputChange = e => {
-        const { name, value } = e.currentTarget;
-        
-        switch (name) {
-            case 'email':
-                return setEmail(value);
-            case 'password':
-                return setPassword(value);
-            default: return;
-        }
-    };
-
-    const onFormSubmit = e => {
-        e.preventDefault();
-//dispatch actions
-        setEmail('');
-        setPassword('');
-    };
-
-    return (<form onSubmit={onFormSubmit}>
-        <label>Email
-            <input  type="text" name="email" required value={ email} onChange={onInputChange}/>
-        </label>
-        <label>Password
-            <input type="password" name="password" required value={ password} onChange={onInputChange}/>
-        </label>
-        <button type="submit">Register</button>
-    </form>)
+    return (<LoginForm/>)
 }
 
 export default LoginPage;
